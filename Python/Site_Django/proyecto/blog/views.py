08190d066c73611e498e3e57fc01e7961f216a8d
Julio César Palacio Cano <dh4rm4n@MacBook.local>
2022-09-20 19:56:22 -0500

    Se agrega proyecto Python-Site_Django

---
+++ Python/Site_Django/proyecto/blog/views.py
@@ -0,0 +1,13 @@
+import imp
+from django.shortcuts import render, HttpResponse
+from .models import Post
+
+# Create your views here.
+def home(request):
+    blogs = Post.objects.all()
+    return render(request, "blog/home.html", {'blogs':blogs})
+
+def blog(request, id):
+    blog = Post.objects.get(id=id)
+    return render(request, "blog/blog.html", {'blog':blog})
+    
